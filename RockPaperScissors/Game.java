a45a343c2dc92883436b17b62af7d8937ef1981f
MV032 <133729266+MV032@users.noreply.github.com>
2024-12-14 13:25:34 -0700

    Add files via upload

---
+++ RockPaperScissors/Game.java
@@ -0,0 +1,6 @@
+public class Game{
+    public static void main(String[] args){
+        Player playerOne = new Player("PlayerOne", true);
+        System.out.println(playerOne.getScore());
+    }
+}
